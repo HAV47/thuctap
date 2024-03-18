f59fdaea31ade7bcc48d6dd892631a9b86e6a111
Hoàng Anh Vũ <HAV47>
2024-03-19 00:38:04 +0700

    first commit

---
+++ android/app/src/main/java/com/example/havv/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.havv;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ứng Dụng Nghe Nhạc',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ứng dụng nghe nhạc'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Tìm kiếm...',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          ),
+          DefaultTabController(
+            length: 3,
+            child: Column(
+              children: [
+                TabBar(
+                  tabs: [
+                    Tab(text: 'All'),
+                    Tab(text: 'Album'),
+                    Tab(text: 'Playlist'),
+                  ],
+                ),
+                Container(
+                  height: 450,
+                  child: TabBarView(
+                    children: [
+                      Center(
+                        child: Text('Danh sách bài hát All'),
+                      ),
+                      Center(
+                        child: Text('Danh sách album'),
+                      ),
+                      Center(
+                        child: Text('Danh sách playlist'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
